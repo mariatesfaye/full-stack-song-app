a19bf58aaad532035a15d3cfd171e305d6d2f0a4
mariatesfaye <120381823+mariatesfaye@users.noreply.github.com>
2024-02-11 17:54:22 +0300

    fixed it

--- src/components/SongUpdate.js
+++ src/components/SongUpdate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateSong } from '../redux/slices/songsSlice';
+import { updateSong } from '../redux/slices/songsSlice'; 
 import SongForm from './SongForm';
 
 const SongUpdate = ({ match }) => {
