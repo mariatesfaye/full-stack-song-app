7dd80a314a58e90b4fc0946a0195756d29a1cb2d
mariatesfaye <120381823+mariatesfaye@users.noreply.github.com>
2024-02-11 18:13:14 +0300

    react crud-song

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-  ],
+  ], 
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
